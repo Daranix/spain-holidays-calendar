ff87fb0c5c4e72b4e5b98ecdd73876a0998cdce0
Esteban Mayoral <contact@mpesteban.dev>
2024-04-20 00:59:49 +0200

    Add step to deploy to fly.io and generate docker image

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,30 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
+const basicConfig: CapacitorConfig = {
   appId: 'dev.mpesteban.calendario.festivos',
   appName: 'Calendario Festivos España',
   webDir: 'dist/browser',
-  server: {
-    cleartext: true,
-    url: 'http://10.0.2.2:4200',
-    androidScheme: 'https'
+}
+
+function getConfig(): CapacitorConfig {
+  if(process.env['IS_BUILD'] === 'true') {
+    return {
+      ...basicConfig
+    };
   }
-};
+
+  return {
+    ...basicConfig,
+    server: {
+      cleartext: true,
+      url: 'http://10.0.2.2:4200',
+      androidScheme: 'https'
+    }
+  };
+}
+
+const config: CapacitorConfig = getConfig();
+
+
 
 export default config;
