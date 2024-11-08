47cab5f64f84e96110add97c79b829a77821731a
Esteban Mayoral Poncelas <emponcelas@izertis.com>
2024-11-08 12:49:34 +0100

    Fix duplicated cannonical tag.

--- src/app/utils/common.ts
+++ src/app/utils/common.ts
@@ -4,8 +4,13 @@ import { PLATFORM_ID, inject } from "@angular/core";
 export function updateCanonnicalUrl(url?: string) {
     const document = inject(DOCUMENT);
     const canURL = !url ? document.URL : url;
-    const link: HTMLLinkElement = document.createElement('link');
-    link.setAttribute('rel', 'canonical');
-    document.head.appendChild(link);
-    link.setAttribute('href', canURL);
+    const canonicalElement = document.head.querySelector('link[rel="canonical"]');
+    if(canonicalElement) {
+        canonicalElement.setAttribute('href', canURL);
+    } else {
+        const link: HTMLLinkElement = document.createElement('link');
+        link.setAttribute('rel', 'canonical');
+        document.head.appendChild(link);
+        link.setAttribute('href', canURL);
+    }
 }
